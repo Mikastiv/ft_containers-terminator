038ab5bf4d9bca7e2c8b1bc9943933f5f77ec33f
mikastiv <mikastiv@outlook.com>
2022-05-08 20:50:54 -0400

    setup

---
+++ tests/prelude.hpp
@@ -0,0 +1,79 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   prelude.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mleblanc <mleblanc@student.42quebec.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/08 20:35:37 by mleblanc          #+#    #+#             */
+/*   Updated: 2022/05/08 20:44:44 by mleblanc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include "track/track_allocator.hpp"
+#include "track/leak_checker.hpp"
+
+#define PRINT_FILE_LINE()                                                                          \
+    {                                                                                              \
+        std::cout << " (file: " << __FILE__ << ", line: " << __LINE__ << ")\n";                    \
+    }
+
+#define PRINT_LINE(msg, value)                                                                     \
+    {                                                                                              \
+        std::cout << msg << " " << value;                                                          \
+        PRINT_FILE_LINE();                                                                         \
+    }
+
+#define PRINT_MSG(msg)                                                                             \
+    {                                                                                              \
+        std::cout << msg;                                                                          \
+        PRINT_FILE_LINE();                                                                         \
+    }
+
+template <typename Iter>
+void print_vector(Iter first, Iter last)
+{
+    for (Iter it = first; it != last; ++it) {
+        std::cout << *it << " ";
+    }
+    std::cout << "\n";
+}
+
+#define PRINT_VEC(vec)                                                                             \
+    {                                                                                              \
+        print_vector(vec.begin(), vec.end());                                                      \
+    }
+
+#define PRINT_SIZE_CAP(vec)                                                                        \
+    {                                                                                              \
+        PRINT_LINE("Size:", vec.size());                                                           \
+        PRINT_LINE("Capacity:", vec.capacity());                                                   \
+    }
+
+#define PRINT_ALL(vec)                                                                             \
+    {                                                                                              \
+        PRINT_SIZE_CAP(vec);                                                                       \
+        PRINT_VEC(vec);                                                                            \
+    }
+
+#define CATCH_UNHANDLED_EX()                                                                       \
+    catch (...)                                                                                    \
+    {                                                                                              \
+        std::cout << "unhandled exception";                                                        \
+        PRINT_FILE_LINE();                                                                         \
+    }
+
+template <typename ForwardIt, typename T>
+void iota(ForwardIt first, ForwardIt last, T value = T())
+{
+    while (first != last) {
+        *first++ = value;
+        ++value;
+    }
+}
+
+#ifndef NAMESPACE
+#define NAMESPACE ft
+#endif
